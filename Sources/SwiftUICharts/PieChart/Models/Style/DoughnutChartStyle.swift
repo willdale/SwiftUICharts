c6faa463223b52a970e60976485c886e7738a5a9
Will Dale <w.dale@me.com>
2021-03-12 08:00:58 +0000

    Add background colour to info box.

--- Sources/SwiftUICharts/PieChart/Models/Style/DoughnutChartStyle.swift
+++ Sources/SwiftUICharts/PieChart/Models/Style/DoughnutChartStyle.swift
@@ -21,6 +21,7 @@ public struct DoughnutChartStyle: CTDoughnutChartStyle {
     public var infoBoxPlacement         : InfoBoxPlacement
     public var infoBoxValueColour       : Color
     public var infoBoxDescriptionColour : Color
+    public var infoBoxBackgroundColour  : Color
     
     public var globalAnimation  : Animation
     
@@ -31,17 +32,21 @@ public struct DoughnutChartStyle: CTDoughnutChartStyle {
     ///   - infoBoxPlacement: Placement of the information box that appears on touch input.
     ///   - infoBoxValueColour: Colour of the value part of the touch info.
     ///   - infoBoxDescriptionColour: Colour of the description part of the touch info.
+    ///   - infoBoxBackgroundColour: Background colour of touch info.
     ///   - globalAnimation: Global control of animations.
     ///   - strokeWidth: Width / Delta of the Doughnut Chart
     public init(infoBoxPlacement        : InfoBoxPlacement  = .floating,
                 infoBoxValueColour      : Color             = Color.primary,
                 infoBoxDescriptionColour: Color             = Color.primary,
+                infoBoxBackgroundColour : Color             = Color.systemsBackground,
+                
                 globalAnimation         : Animation         = Animation.linear(duration: 1),
                 strokeWidth             : CGFloat           = 30
     ) {
         self.infoBoxPlacement         = infoBoxPlacement
         self.infoBoxValueColour       = infoBoxValueColour
         self.infoBoxDescriptionColour = infoBoxDescriptionColour
+        self.infoBoxBackgroundColour  = infoBoxBackgroundColour
         self.globalAnimation          = globalAnimation
         self.strokeWidth              = strokeWidth
     }
--- Sources/SwiftUICharts/PieChart/Models/Style/PieChartStyle.swift
+++ Sources/SwiftUICharts/PieChart/Models/Style/PieChartStyle.swift
@@ -21,7 +21,8 @@ public struct PieChartStyle: CTPieChartStyle {
         
     public var infoBoxPlacement         : InfoBoxPlacement
     public var infoBoxValueColour       : Color
-    public var infoBoxDescriptionColour  : Color
+    public var infoBoxDescriptionColour : Color
+    public var infoBoxBackgroundColour  : Color
     
     public var globalAnimation : Animation
     
@@ -30,15 +31,18 @@ public struct PieChartStyle: CTPieChartStyle {
     ///   - infoBoxPlacement: Placement of the information box that appears on touch input.
     ///   - infoBoxValueColour: Colour of the value part of the touch info.
     ///   - infoBoxDescriptionColour: Colour of the description part of the touch info.
+    ///   - infoBoxBackgroundColour: Background colour of touch info.
     ///   - globalAnimation: Global control of animations.
     public init(infoBoxPlacement        : InfoBoxPlacement  = .floating,
                 infoBoxValueColour      : Color             = Color.primary,
                 infoBoxDescriptionColour: Color             = Color.primary,
+                infoBoxBackgroundColour : Color             = Color.systemsBackground,
                 globalAnimation         : Animation         = Animation.linear(duration: 1)
     ) {
         self.infoBoxPlacement         = infoBoxPlacement
         self.infoBoxValueColour       = infoBoxValueColour
         self.infoBoxDescriptionColour = infoBoxDescriptionColour
+        self.infoBoxBackgroundColour  = infoBoxBackgroundColour
         self.globalAnimation          = globalAnimation
     }
 }
