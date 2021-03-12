79ff73fdbcfa31dc817e29a109a540a2ef750c49
Will Dale <w.dale@me.com>
2021-03-12 08:55:27 +0000

    Refactor.

--- Sources/SwiftUICharts/SharedLineAndBar/Models/Protocols/LineAndBarProtocolsExtentions.swift
+++ Sources/SwiftUICharts/SharedLineAndBar/Models/Protocols/LineAndBarProtocolsExtentions.swift
@@ -73,23 +73,3 @@ extension CTLineBarChartDataProtocol {
         return labels
     }
 }
-
-extension CTChartData {
-    /// Sets the point info box location while keeping it within the parent view.
-    /// - Parameters:
-    ///   - boxFrame: The size of the point info box.
-    ///   - chartSize: The size of the chart view as the parent view.
-    internal func setBoxLocationation(touchLocation: CGFloat, boxFrame: CGRect, chartSize: CGRect) -> CGFloat {
-
-        var returnPoint : CGFloat = .zero
-
-        if touchLocation < chartSize.minX + (boxFrame.width / 2) {
-            returnPoint = chartSize.minX + (boxFrame.width / 2)
-        } else if touchLocation > chartSize.maxX - (boxFrame.width / 2) {
-            returnPoint = chartSize.maxX - (boxFrame.width / 2)
-        } else {
-            returnPoint = touchLocation
-        }
-        return returnPoint + self.infoView.yAxisLabelWidth
-    }
-}
