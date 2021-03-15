d97eef763d78692c9e25234d50ed77e7b49dbff7
Will Dale <63838770+willdale@users.noreply.github.com>
2021-03-15 15:12:30 +0000

    Tests update.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,17 +1,8 @@
 import XCTest
 
-@testable import BarChartTests
-@testable import GroupedBarChartTests
-@testable import StackedBarChartTests
-@testable import LineChartTests
-@testable import MultiLineChartTest
-@testable import LineChartPathTests
+import SwiftUIChartsTests
 
 var tests = [XCTestCaseEntry]()
-tests += BarChartTests.allTests()
-tests += GroupedBarChartTests.allTests()
-tests += StackedBarChartTests.allTests()
-tests += LineChartTests.allTests()
-tests += MultiLineChartTest.allTests()
-tests += LineChartPathTests.allTests()
+tests += SwiftUIChartsTests.__allTests()
+
 XCTMain(tests)
