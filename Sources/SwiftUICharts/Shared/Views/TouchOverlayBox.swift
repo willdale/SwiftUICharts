fb50c4a85fed3b52a2f1522d5598f3f985ef4b0b
xremix <tonidhoffmann@gmail.com>
2021-03-29 22:49:17 +0200

    make use of different alignments

--- Sources/SwiftUICharts/Shared/Views/TouchOverlayBox.swift
+++ Sources/SwiftUICharts/Shared/Views/TouchOverlayBox.swift
@@ -27,7 +27,7 @@ internal struct TouchOverlayBox<T: CTChartData>: View {
         
         VStack(alignment: .leading, spacing: 0) {
             ForEach(chartData.infoView.touchOverlayInfo, id: \.id) { point in
-                if true{
+                if chartData.chartStyle.infoBoxContentAlignment == .vertical{
                     chartData.infoDescription(info: point)
                         .font(chartData.chartStyle.infoBoxDescriptionFont)
                         .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
@@ -50,7 +50,7 @@ internal struct TouchOverlayBox<T: CTChartData>: View {
                         chartData.infoValueUnit(info: point)
                             .font(chartData.chartStyle.infoBoxValueFont)
                             .foregroundColor(chartData.chartStyle.infoBoxValueColour)
-                    }                    
+                    }
                 }
             }
         }
