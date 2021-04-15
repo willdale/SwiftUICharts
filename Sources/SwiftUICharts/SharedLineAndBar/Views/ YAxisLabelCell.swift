5efd625acf577a8e77417dacab3c7322dc6f9a2a
Will Dale <63838770+willdale@users.noreply.github.com>
2021-04-15 13:56:03 +0100

    Fix Label Layout.

--- Sources/SwiftUICharts/SharedLineAndBar/Views/ YAxisLabelCell.swift
+++ Sources/SwiftUICharts/SharedLineAndBar/Views/ YAxisLabelCell.swift
@@ -51,30 +51,32 @@ internal struct XAxisChartDataCell<ChartData>: View where ChartData: CTLineBarCh
     private let label: String
     private let rotationAngle: Angle
     
-    internal init(chartData: ChartData, label: String, rotationAngle : Angle) {
+    internal init(chartData: ChartData, label: String, rotationAngle: Angle) {
         self.chartData     = chartData
         self.label         = label
         self.rotationAngle = rotationAngle
     }
     
     @State private var width: CGFloat = 0
- 
+    @State private var height: CGFloat = 0
+    
     internal var body: some View {
 
         Text(label)
             .font(chartData.chartStyle.xAxisLabelFont)
             .lineLimit(1)
+            .fixedSize(horizontal: true, vertical: false)
+            .rotationEffect(rotationAngle, anchor: .top)
             .overlay(
                 GeometryReader { geo in
                     Color.clear
                         .onAppear {
                             self.width = geo.frame(in: .local).width
+                            self.height = geo.frame(in: .local).height
                         }
                 }
             )
-            .fixedSize(horizontal: true, vertical: false)
-            .rotationEffect(rotationAngle, anchor: .center)
-            .frame(height: width, alignment: .top)
+            .frame(width: width, height: rotationAngle == .init(degrees: 0) || rotationAngle == .init(radians: 0) ? height : width)
             .onAppear {
                 chartData.viewData.xAxisLabelHeights.append(width)
             }
