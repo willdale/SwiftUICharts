5d67ee8cbef033319c3ad549f9268673e4b303de
Will Dale <63838770+willdale@users.noreply.github.com>
2021-05-09 15:48:39 +0100

    Update GetYLabels functions tests.

--- Tests/SwiftUIChartsTests/BarCharts/BarChartTests.swift
+++ Tests/SwiftUIChartsTests/BarCharts/BarChartTests.swift
@@ -38,28 +38,29 @@ final class BarChartTests: XCTestCase {
         
         let chartData = BarChartData(dataSets: BarDataSet(dataPoints: dataPoints),
                                      chartStyle: BarChartStyle(yAxisNumberOfLabels: 3))
+        chartData.viewData.yAxisSpecifier = "%.2f"
         
         chartData.chartStyle.topLine  = .maximumValue
         chartData.chartStyle.baseline = .zero
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "0.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "30.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "60.00")
+        XCTAssertEqual(chartData.labelsArray[0], "0.00")
+        XCTAssertEqual(chartData.labelsArray[1], "30.00")
+        XCTAssertEqual(chartData.labelsArray[2], "60.00")
         
         chartData.chartStyle.baseline = .minimumValue
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "10.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "35.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "60.00")
+        XCTAssertEqual(chartData.labelsArray[0], "10.00")
+        XCTAssertEqual(chartData.labelsArray[1], "35.00")
+        XCTAssertEqual(chartData.labelsArray[2], "60.00")
         
         chartData.chartStyle.baseline = .minimumWithMaximum(of: 5)
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "5.00" )
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "32.50")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "60.00")
+        XCTAssertEqual(chartData.labelsArray[0], "5.00" )
+        XCTAssertEqual(chartData.labelsArray[1], "32.50")
+        XCTAssertEqual(chartData.labelsArray[2], "60.00")
         
         chartData.chartStyle.topLine  = .maximum(of: 100)
         chartData.chartStyle.baseline = .zero
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "0.00"  )
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "50.00" )
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "100.00")
+        XCTAssertEqual(chartData.labelsArray[0], "0.00"  )
+        XCTAssertEqual(chartData.labelsArray[1], "50.00" )
+        XCTAssertEqual(chartData.labelsArray[2], "100.00")
     }
     
     // MARK: - Touch
--- Tests/SwiftUIChartsTests/LineCharts/LineChartTests.swift
+++ Tests/SwiftUIChartsTests/LineCharts/LineChartTests.swift
@@ -45,28 +45,29 @@ final class LineChartTests: XCTestCase {
     func testLineGetYLabels() {
         let chartData = LineChartData(dataSets: LineDataSet(dataPoints: dataPoints),
                                       chartStyle: LineChartStyle(yAxisNumberOfLabels: 3))
+        chartData.viewData.yAxisSpecifier = "%.2f"
         
         chartData.chartStyle.topLine  = .maximumValue
         chartData.chartStyle.baseline = .zero
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "0.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "40.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "80.00")
+        XCTAssertEqual(chartData.labelsArray[0], "0.00")
+        XCTAssertEqual(chartData.labelsArray[1], "40.00")
+        XCTAssertEqual(chartData.labelsArray[2], "80.00")
         
         chartData.chartStyle.baseline = .minimumValue
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "10.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "45.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "80.00")
+        XCTAssertEqual(chartData.labelsArray[0], "10.00")
+        XCTAssertEqual(chartData.labelsArray[1], "45.00")
+        XCTAssertEqual(chartData.labelsArray[2], "80.00")
         
         chartData.chartStyle.baseline = .minimumWithMaximum(of: 5)
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "5.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "42.50")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "80.00")
+        XCTAssertEqual(chartData.labelsArray[0], "5.00")
+        XCTAssertEqual(chartData.labelsArray[1], "42.50")
+        XCTAssertEqual(chartData.labelsArray[2], "80.00")
         
         chartData.chartStyle.topLine  = .maximum(of: 100)
         chartData.chartStyle.baseline = .zero
-        XCTAssertEqual(chartData.getYLabels("%.2f")[0], "0.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[1], "50.00")
-        XCTAssertEqual(chartData.getYLabels("%.2f")[2], "100.00")
+        XCTAssertEqual(chartData.labelsArray[0], "0.00")
+        XCTAssertEqual(chartData.labelsArray[1], "50.00")
+        XCTAssertEqual(chartData.labelsArray[2], "100.00")
     }
     
     
