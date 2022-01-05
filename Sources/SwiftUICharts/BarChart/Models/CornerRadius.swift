deb98fcf6cad474da52c62845d2786bccc1c78db
Kunal Verma <k>
2022-01-05 11:45:28 +0530

    Enabling corner radius for left/right and explicitly setting all corners

--- Sources/SwiftUICharts/BarChart/Models/CornerRadius.swift
+++ Sources/SwiftUICharts/BarChart/Models/CornerRadius.swift
@@ -12,15 +12,43 @@ import SwiftUI
  */
 public struct CornerRadius: Hashable {
     
-    public let top: CGFloat
-    public let bottom: CGFloat
+    public let topLeft: CGFloat
+    public let topRight: CGFloat
+    public let bottomLeft: CGFloat
+    public let bottomRight: CGFloat
     
-    /// Set the coner radius for the bar shapes
+    /// Set the coner radius for the bar shapes for top and bottom
     public init(
         top: CGFloat = 15.0,
         bottom: CGFloat = 0.0
     ) {
-        self.top = top
-        self.bottom = bottom
+        self.topLeft = top
+        self.topRight = top
+        self.bottomLeft = bottom
+        self.bottomRight = bottom
+    }
+    
+    /// Set the coner radius for the bar shapes for left and right
+    public init(
+        left: CGFloat = 0.0,
+        right: CGFloat = 0.0
+    ) {
+        self.topLeft = left
+        self.topRight = right
+        self.bottomLeft = left
+        self.bottomRight = right
+    }
+    
+    /// Set the coner radius for the bar shapes for all corners
+    public init(
+        topLeft: CGFloat = 0,
+        topRight: CGFloat = 0,
+        bottomLeft: CGFloat = 0,
+        bottomRight: CGFloat = 0
+    ) {
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
     }
 }
